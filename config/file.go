8fcb86bbba7950933a797db1158f8ae1e6e9bc18
Daniel Hess <dan9186@gmail.com>
2020-03-13 21:46:56 -0700

    explicitly match only the set route, and add more feedback

--- config/file.go
+++ config/file.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"strings"
 
 	"github.com/gomicro/ledger"
 	"gopkg.in/yaml.v2"
@@ -97,15 +96,8 @@ func (f *File) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if strings.Contains(r.URL.Path, "/set") {
-
-		queryValues := r.URL.Query()
-
-		for k := range f.routesMap {
-			f.routesMap[k].Set(queryValues.Get("name"), queryValues.Get("id"))
-		}
-
-		w.WriteHeader(http.StatusOK)
+	if r.URL.Path == f.Set {
+		handleSet(w, r, f)
 		return
 	}
 
@@ -132,4 +124,35 @@ func (f *File) getRoute(reqURL *url.URL) (*Route, bool) {
 func handleStatus(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("duty is functioning"))
+}
+
+func handleSet(w http.ResponseWriter, req *http.Request, f *File) {
+	log.Debug("setting endpoint")
+
+	name := req.URL.Query().Get("name")
+	id := req.URL.Query().Get("id")
+
+	if name == "" || id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("name and id are required query params"))
+		return
+	}
+
+	for k := range f.routesMap {
+		if f.routesMap[k].Name == name {
+			err := f.routesMap[k].Set(id)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(fmt.Sprintf("failed to set route: %v", err.Error())))
+				return
+			}
+
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte("no route found"))
+	return
 }
